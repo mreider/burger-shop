70fef7004adf8cecbabcfe52e3f343081477a115
elinest1105 <james.cobb812@gmail.com>
2024-08-19 03:16:07 -0400

    removed REST endpoint from fulfillmentcontroller

--- fulfillmentservice/src/main/java/com/example/fulfillmentservice/service/FullfillmentService.java
+++ fulfillmentservice/src/main/java/com/example/fulfillmentservice/service/FullfillmentService.java
@@ -3,11 +3,8 @@ package com.example.fulfillmentservice.service;
 import com.example.fulfillmentservice.model.Order;
 import com.example.fulfillmentservice.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import java.util.concurrent.CompletableFuture;
 
 /*
 @Service
