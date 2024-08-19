e8d68971e66f9226d3c7c4a1cbe201a18212b30b
elinest1105 <james.cobb812@gmail.com>
2024-08-19 04:57:05 -0400

    added RabbitMQConfig to fulFillmentService

---
+++ fulfillmentservice/src/main/java/com/example/fulfillmentservice/config/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.example.fulfillmentservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    static final String queueName = "orderQueue";
+    static final String exchangeName = "orderExchange";
+    static final String routingKey = "orderRoutingKey";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+}
